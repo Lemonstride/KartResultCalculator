c9971209c0cd5b096bb95e8e595f2d2080951f8e
神楽坂楓 <ice@ice.local>
2025-03-20 14:41:52 +0800

    fixed name bugs

--- module/__init__.py
+++ module/__init__.py
@@ -1,4 +1,4 @@
-from .save_result import calculate_and_save, clear_scores, load_scores
+from .save_results import save_to_csv, clear_scores, load_scores
 from .crop import crop_image
 from .preprocess import preprocess_image
 from .ocr_processing import ocr_and_process
